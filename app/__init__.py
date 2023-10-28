df5c7d2bdbebd0e03de101bd2305f64fc12275a4
sofiane <abdellaoui.sofiane.esb@gmail.com>
2023-10-28 15:53:14 +0200

    add config for sqlite db

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,27 @@ from flask import Flask
 def create_app():
 
     # Init flask app
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
 
     # Config app
-    app.config.from_mapping(SECRET_KEY="Dummy-Secret-KEY")
+    app.config.from_mapping(
+        SECRET_KEY="Dummy-Secret-KEY",
+        DATABASE=os.path.join(app.instance_path, "database.sqlite"))
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     # Register index route
     @app.route('/')
     def hello():
         response = json.dumps({"message": "I'm alive !!"})
         return response, 200, {'Content-Type': 'application/json'}
+
+    # Init database
+    from . import db
+    db.init_app(app)
 
     return app
